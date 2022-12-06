9a411d8746d2e4cae4f8e4cb075bf82cfc935e69
dadutchboy2 <48090115+dadutchboy2@users.noreply.github.com>
2022-12-06 13:49:01 +0100

    Create 3-1.lua

---
+++ 3-1.lua
@@ -0,0 +1,31 @@
+inventory = require(script.ModuleScript)
+
+rucksacks = inventory:split("\n")
+
+totalPriority = 0
+
+function addPriority(character)
+	local byte = character:byte()
+	
+	if byte >= 97 and byte <= 122 then
+		totalPriority += byte - 96
+	else
+		totalPriority += byte - 65 + 27
+	end
+end
+
+for _, rucksack in rucksacks do
+	local length = rucksack:len()
+	
+	local compartment1, compartment2 = rucksack:sub(1, length / 2), rucksack:sub(length / 2 + 1, -1)
+	
+	for _, character in compartment2:split("") do
+		if not compartment1:match(character) then continue end
+		
+		addPriority(character)
+		
+		break
+	end
+end
+
+print(totalPriority)
