49e82e6be4b48084c653731d884344c7821ad7bf
dadutchboy2 <48090115+dadutchboy2@users.noreply.github.com>
2022-12-09 10:45:18 +0100

    Create 2015-1.java

---
+++ 2015-1.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int floor = 0;
+        int i = 0;
+        String instructionString = "insert input here";
+        char[] instructions = instructionString.toCharArray();
+        for (i = 1; i <= instructions.length; i++) {
+            char instruction = instructions[i - 1];
+            floor += instruction == '(' ? 1 : -1;
+            if (floor == -1) {
+                break;
+            }
+        }
+        System.out.println(i);
+    }
+}
+//java sucks and ill avoid it for the rest of my life
